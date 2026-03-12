d516d069e84ddaba4ac2e2cd9ff6f8d3c193ea11
Tomas Becza <beczatom@fit.cvut.cz>
2026-03-12 10:43:41 +0100

    Resampling experiment

---
+++ scripts/resampling.py
@@ -0,0 +1,69 @@
+
+import torch
+import xarray
+import rasterio
+
+import loss
+
+RESAMPLINGS = {
+    'nearest' : rasterio.enums.Resampling.nearest,
+    'bilinear' : rasterio.enums.Resampling.bilinear,
+    'cubic' : rasterio.enums.Resampling.cubic,
+    'cubic_spline' : rasterio.enums.Resampling.cubic_spline,
+    'lanczos' : rasterio.enums.Resampling.lanczos,
+    'average' : rasterio.enums.Resampling.average
+}
+
+LOSSES = {
+    'conservation' : loss.ConservationLoss(16),
+    'simple gradient' : loss.SoftSimpleGradientLoss(16),
+    'continuity' : loss.SoftContinuityLoss(16),
+    'sobel' : loss.SoftSobelGradientLoss(16),
+    'G-loss' : loss.SoftGLoss(16),
+    'direction continuity' : loss.SoftDirectionContinuityLoss(16),
+}
+
+class ReKIS(torch.utils.data.Dataset):
+
+    def __init__(self, Y: xarray.DataArray, scale_factor : int, resampling):
+        self.scale_factor = scale_factor
+
+        Y = Y.isel(easting=slice(0, 400), northing=slice(0, 400))
+        Y.rio.set_spatial_dims("easting", "northing")
+
+        resolution = 1000 * self.scale_factor
+        X = Y.rio.reproject(
+            Y.rio.crs, resolution=(resolution, resolution), resampling=resampling
+        )
+        self.X = torch.from_numpy(X.values).unsqueeze(1)
+        self.Y = torch.from_numpy(Y.values).unsqueeze(1)
+
+
+    def __len__(self) -> int:
+        return len(self.Y)
+
+    def __getitem__(self, index: int):
+        return self.X[index], self.Y[index]
+
+if __name__ == "__main__":
+    PATH = '/mnt/data/climate/ReKIS/KlimRefDS_v3.1_1961-2023/Raster/Tag/GK4/'
+
+    Y_raw = xarray.open_mfdataset(PATH + "TM/*.nc", decode_coords="all")
+    Y_raw = Y_raw["TM"]
+
+    for resampling_name, resampling in RESAMPLINGS.items():
+        dataset = ReKIS(Y_raw.sel(time=slice('1961', '1992')).copy(), 16, resampling=resampling)
+
+        X, Y = dataset.X.reshape((-1, 1, 400 // 16, 400 // 16)), dataset.Y.reshape((-1, 1, 400, 400))
+
+        print(50 * '=')
+        print(resampling_name)
+
+        X_upscaled = torch.kron(X, torch.ones(16, 16))
+        print(X_upscaled.shape)
+
+        print('RMSE:', ((X_upscaled - Y) ** 2).mean().sqrt())
+        print('MAE:', (X_upscaled - Y).abs().mean())
+
+        for loss_name, loss_fn in LOSSES.items():
+            print(f'{loss_name}: {loss_fn(Y, None, X)}')
