0971abb3e4e1a088042be6bada6fd4f784c0346b
Tomas Becza <beczatom@fit.cvut.cz>
2026-04-11 23:16:06 +0200

    Average resampling

--- src/datasets/rekis.py
+++ src/datasets/rekis.py
@@ -10,7 +10,7 @@ import torch
 import xarray
 
 # Average resampling used to reduce resolution
-RESAMPLING = rasterio.enums.Resampling.cubic_spline
+RESAMPLING = rasterio.enums.Resampling.average
 
 
 class ReKIS(torch.utils.data.Dataset):
